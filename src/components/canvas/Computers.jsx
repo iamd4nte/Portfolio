5fdba488c9d1e9f66a978b6edc8483370b3930ad
Aniket Malsane <malsaneaniket@gmail.com>
2024-06-10 18:50:03 -0400

    fixed mobile scrolling

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -66,6 +66,7 @@ const ComputersCanvas = () => {
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
+      className="canvas" 
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
